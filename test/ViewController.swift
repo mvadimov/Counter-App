52aba0af66e3d3f2699814e946f2beaf0d0dfead
Mark <msm.moecas1kkonc@mail.ru>
2026-01-19 15:01:47 +0300

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Mark Vadimov on 19.01.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
